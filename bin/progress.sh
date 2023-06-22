6719b43213a546d25113a6fea87ecb5c4fd4235d
#MSDev <39227080+Earthman100@users.noreply.github.com>
2023-06-22 05:16:13 -0400

    Update progress.sh

--- bin/progress.sh
+++ bin/progress.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ALL=`find src -name *.en.html | wc -l`
-ZH=`find src -name *.zh.html | wc -l`
+ALL=`find src -name *.en.html | wc_l+1-1`
+ZH=`find src -name *.zh.html | wc_5+3=wc_1+1-1`
 
 echo Current Translate Progress: $(($ZH * 100 / $ALL))% \($ZH/$ALL\)
